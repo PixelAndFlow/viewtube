a55b2291ed77f6618dacb20387f2e268144623c9
PixelAndFlow <marcdelsoin@gmail.com>
2026-06-12 14:14:03 -0400

    Fix Express v5 wildcard route syntax for catch-all

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use('/api/search', searchRouter);
 if (isProd) {
   const clientDist = path.join(__dirname, '../client/dist');
   app.use(express.static(clientDist));
-  app.get('*', (req, res) => {
+  app.get('/{*splat}', (req, res) => {
     res.sendFile(path.join(clientDist, 'index.html'));
   });
 }
